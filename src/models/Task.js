69dcd8bbb17351aa01207ab1c0ed36fca0ff088b
Aristidelove <youfangtilibouaristiderael@gmail.com>
2025-05-27 14:30:00 +0200

    modification

---
+++ src/models/Task.js
@@ -0,0 +1,13 @@
+class Task {
+  constructor(id, text, priority, status, date) {
+    this.id = id;
+    this.text = text;
+    this.priority = priority;
+    this.status = status;
+    this.date = date;
+  }
+}
+
+export default Task;
+
+
