3ad29b5f803dd767a65964ce2c5b8e307962b32d
Aristidelove <youfangtilibouaristiderael@gmail.com>
2025-06-04 08:59:14 +0200

    modification

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import TaskComponent from './COMPONENT';
+
+const Accueil = () => {
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100" >
+      <div className="text-center">
+        <h1 className="display-5 fw-bold fst-italic ">BIENVENUE DANS VOTRE GESTIONNAIRE DE TÂCHES</h1>
+        <Link to="/tasks" style={{ color: 'blue', textDecoration: 'underline' }} className="btn btn-link mt-3">Voir les tâches</Link>
+      </div>
+    </div>
+  );
+};
 
 const App = () => {
   return (
-    <div className="text-center mt-5">
-      <h1 className="display-5 fw-bold">BIENVENUE DANS VOTRE GESTIONNAIRE DE TÂCHES</h1>
-      <TaskComponent />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Accueil />} />
+        <Route path="/tasks" element={<TaskComponent />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
